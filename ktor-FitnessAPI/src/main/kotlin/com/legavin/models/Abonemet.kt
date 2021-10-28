848113715635166e51cd588c5fe43966555eeb24
Vladislav Legavin <vldikk12@gmail.com>
2021-10-28 21:04:00 +0300

    First

--- ktor-FitnessAPI/src/main/kotlin/com/legavin/models/Abonemet.kt
+++ ktor-FitnessAPI/src/main/kotlin/com/legavin/models/Abonemet.kt
@@ -10,22 +10,22 @@ import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.`java-time`.datetime
 import java.time.LocalDateTime
 
-object Abonements: IntIdTable("Abonement", "abonement_id"){
-    val abonement: Column<String> = varchar("abonement", 50)
+object Abonements: IntIdTable("Abonement", "id_abonement"){
+//    val abonement: Column<String> = varchar("abonement", 50)
     val nameAbonement: Column<String> = varchar("nameAbonement", 50)
     val costAbonement: Column<String> = varchar("costAbonement", 50)
     val dateStart: Column<LocalDateTime> = datetime("dateStart")
     val dateFinish: Column<LocalDateTime> = datetime("dateFinish")
 
     //forKey
-   val bonus = reference("bonus", BonusPrograms)
+   val bonus = reference("bonusprogram_id", BonusPrograms)
 
 }
 
 class AbonementEntity(id: EntityID<Int>) : IntEntity(id){
     companion object : IntEntityClass<AbonementEntity>(Abonements)
 
-    var abonement by Abonements.abonement
+//    var abonement by Abonements.abonement
     var nameAbonement by Abonements.nameAbonement
     var costAbonement by Abonements.costAbonement
     var dateStart by Abonements.dateStart
@@ -39,7 +39,7 @@ class AbonementEntity(id: EntityID<Int>) : IntEntity(id){
     @Serializable
     data class Abonemet(@Transient val model: AbonementEntity? = null){
         val id = model!!.id.value
-        val abonement = model!!.abonement
+//        val abonement = model!!.abonement
         val nameAbonement = model!!.nameAbonement
         val costAbonement = model!!.costAbonement
         val dateStart =  model!!.dateStart.toString()
